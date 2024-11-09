6386829ed294e0654c0f4a41f7c9006dc9b71ed2
MatiasOrtega <mat-1998-ort@hotmail.com>
2024-11-09 18:19:23 -0300

    Add static file serving and routing for frontend build in server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
+import path from 'path';
 
 import { connectToMongoDB } from './db/connetTotMongoDB.js'; 
 
@@ -11,6 +12,8 @@ import userRotues from './routes/user.routes.js';
 import { app, server } from './socket/socket.js';
 const port = process.env.PORT_SERVER || 3001;
 
+const __dirname = path.resolve();
+
 config();
 
 app.use(express.json());
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRotues)
 app.use('/api/user', userRotues)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend','dist','index.html'));
+});
 
 server.listen(port, () => {
   connectToMongoDB();
